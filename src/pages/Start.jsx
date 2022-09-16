5fc324a24cb6b05973fddac41fe262c54bead4b9
Joel Sanchez <joeleduardosanchezherrera@gmail.com>
2022-09-16 01:00:22 -0500

    se agregaron las imagenes y se mueven los efectos en ellas

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import "../styles/Start.css"
+import MenuElement from '../sections/MenuElement';
+
+
 const Start = () => {
-    return <>
-        <h1>Hola Soy el Incio</h1>
-    </>
+    return <div className="principal">
+        <h1>Busca El Buho</h1>
+        <div className='menu'>
+        <MenuElement nombre="Abogados" clase="abogado" />
+        <MenuElement nombre="Tramites" clase="tramites" />
+
+        </div>
+    </div>
 }
 
-export default Start;
+
+
+
+
+export default Start; 
---
+++ src/sections/MenuElement.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../styles/menuElement.css"
+
+const MenuElement = props => {
+    let {nombre, clase} = props;
+
+    clase+= " menuElement"
+    
+    return  <div className={clase}>
+        <a href="">
+        <div className="filtro">
+        <p>{nombre}</p>
+        </div>
+        </a>
+       
+        
+ 
+    </div>
+       
+}
+
+export default MenuElement;
