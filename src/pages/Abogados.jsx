9528ca0779c0b6656affc725b5cb451e5523b94b
Joel Sanchez <joeleduardosanchezherrera@gmail.com>
2022-09-20 15:05:15 -0500

    se cambia el layout de los abogados y tiene que entrar a ver las resenas y direccion

--- src/pages/Abogados.jsx
+++ src/pages/Abogados.jsx
@@ -11,6 +11,8 @@ const Abogados = () => {
         }
         
         
+        
+        
     </div>
 }
 
--- src/sections/ItemAbogado.jsx
+++ src/sections/ItemAbogado.jsx
@@ -2,21 +2,17 @@ import React from 'react';
 import "../styles/ItemAbogado.css"
 
 const ItemAbogado = (props) => {
-    const {foto, nombre, telefono, direccion, redes} = props;
-    return <div className="itemAbogado">
-        <div className="foto">
-            {foto}
-        </div>
-
-        <div className="texto">
-        <h3>{nombre}</h3>
-        <p>{telefono}</p>
-        <p>{direccion}</p>
-        <p>{redes}</p>
-        </div>
-        
-        
-    </div>
+    const {foto, nombre, id} = props;
+    return <>
+        <a className='itemAbogado' href="">        
+            <div className="foto">
+                <img src={foto} alt="" />
+            </div>
+            <div className="texto">
+                    <h2>{nombre}</h2>
+            </div>
+        </a>
+    </>
 }
 
 export default ItemAbogado;
