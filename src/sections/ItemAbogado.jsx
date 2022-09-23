83294bd8e59e5806d9590b81419777f46f000393
Joel Sanchez <joeleduardosanchezherrera@gmail.com>
2022-09-23 09:41:31 -0500

    se agregaron las resenias a los abogados

--- src/sections/ItemAbogado.jsx
+++ src/sections/ItemAbogado.jsx
@@ -35,24 +35,39 @@ const ItemAbogado = (props) => {
           <p>Dirección: {direccion}</p>
           <p>Telefono: {telefono}</p>
           {red != null &&
-          <>
-          <a href={link}>{red}</a>
-          </>
+            <>
+              <a href={link}>{red}</a>
+            </>
           }
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
+          <div className="ec-stars-wrapper">
+	          <a href="#" data-value="1" title="Votar con 1 estrellas">&#9733;</a>
+	          <a href="#" data-value="2" title="Votar con 2 estrellas">&#9733;</a>
+	          <a href="#" data-value="3" title="Votar con 3 estrellas">&#9733;</a>
+	          <a href="#" data-value="4" title="Votar con 4 estrellas">&#9733;</a>
+	          <a href="#" data-value="5" title="Votar con 5 estrellas">&#9733;</a>
+          </div>
 
-        </Modal.Footer>
-      </Modal>
+          <div className="resena">
+            <textarea name="" id="" cols="30" rows="10" placeholder='Escribe aqui tu resena'></textarea>
+            <Button variant="primary">Publicar</Button>
+          </div>
+
+        
+
+          </Modal.Body>
+
+          <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+          </Modal.Footer>
+        </Modal>
     </>
 
 
 
 
-  );
+                );
 }
 
-export default ItemAbogado;
+                export default ItemAbogado;
