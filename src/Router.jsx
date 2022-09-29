f97e8023bb65fac11dec33457c78645f4aaea671
Joel Sanchez <joeleduardosanchezherrera@gmail.com>
2022-09-28 23:19:28 -0500

    se configuro el router y el css y el build final

--- src/Router.jsx
+++ src/Router.jsx
@@ -5,13 +5,13 @@ import Abogados from "./pages/Abogados"
 import Tramites from "./pages/Tramites"
 import Layout from "./sections/Layout"
 //src > Router.js
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
 const Router = () => {
 
   return (
     
     
-    <BrowserRouter>
+    <HashRouter>
     <Layout>
         <Routes>
           <Route path="/" element={<Start />}></Route>
@@ -19,7 +19,7 @@ const Router = () => {
           <Route path="tramites" element={<Tramites />}></Route>
         </Routes>
     </Layout>
-      </BrowserRouter>
+      </HashRouter>
       );
 };
 export default Router;
