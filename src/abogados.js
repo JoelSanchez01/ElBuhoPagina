dc41db82e91ec1081a45b4b86c514ebc3f00c187
Joel Sanchez <joeleduardosanchezherrera@gmail.com>
2022-09-28 21:30:13 -0500

    se agregaron las iamgenes

--- src/abogados.js
+++ src/abogados.js
@@ -1,7 +1,7 @@
 const datos = [
     {
         "id":1,
-        "foto": "../../src/assets/abogadoexample.jpg",
+        "foto": "/abogado1.jpg",
         "nombre": "Bufete Jurídico Arteaga - BJA ABOGADOS",
         "direccion": "Calle Cristóbal Colon 104 norte zona centro de Tampico Tercer Piso #105 89000 Tampico, Tamps ",
         "telefono": "833 137 1740",
@@ -10,7 +10,7 @@ const datos = [
     },
     {
         "id":2,
-        "foto": "../../src/assets/abogadoexample.jpg",
+        "foto": "/abogado2.jpg",
         "nombre": "Ortiz Barraza Abogados, S.C.",
         "direccion": "Dr. Alfredo Gochicoa 113, Cascajal, 89280 Tampico, Tamps.",
         "telefono": "833 212 3013",
@@ -18,7 +18,7 @@ const datos = [
     },
     {
         "id":3,
-        "foto": "../../src/assets/abogadoexample.jpg",
+        "foto": "/abogado3.jpg",
         "nombre": "Abogados del Trabajo Tampico Madero y Altamira",
         "direccion": "Tienda Milano, Edificio Edith, Calle: Colon N° 104 NORTE, Segundo Piso Despacho N° 207 89000, 89000 Tampico, Tamps.",
         "telefono": "833 307 6427",
@@ -27,7 +27,7 @@ const datos = [
     },
     {
         "id":4,
-        "foto": "../../src/assets/abogadoexample.jpg",
+        "foto": "/abogado4.jpg",
         "nombre":"Despacho Jurídico LEASING",
         "direccion": "Juan M. Torrea 306, Smith, 89140 Tampico, Tamps.",
         "telefono": "833 134 3029",
@@ -35,7 +35,7 @@ const datos = [
     },
     {
         "id":5,
-        "foto": "../../src/assets/abogadoexample.jpg",
+        "foto": "/abogado5.jpg",
         "nombre":"Bufete Jurídico Civil Familiar Agrario Penal",
         "direccion": "C. Tamaulipas Nº 305, Zona Centro, 89000 Tampico, Tamps.",
         "telefono": "833 307 6427",
@@ -43,7 +43,7 @@ const datos = [
     },
     {
         "id":6,
-        "foto": "../../src/assets/abogadoexample.jpg",
+        "foto": "/abogado6.jpg",
         "nombre":"Corporativo Juridico RA",
         "direccion": "Privada Tamaulipas 210 colonia Minerva, 89210 Tampico.",
         "telefono": "833 211 3633",
@@ -51,7 +51,7 @@ const datos = [
     },
     {
         "id":7,
-        "foto": "../../src/assets/abogadoexample.jpg",
+        "foto": "/abogado7.jpg",
         "nombre":"Medina Firma Legal",
         "direccion": "Poniente 308, Zona Centro, 89000 Tampico, Tamps.",
         "telefono": "833 212 3258",
@@ -59,7 +59,7 @@ const datos = [
     },
     {
         "id":8,
-        "foto": "../../src/assets/abogadoexample.jpg",
+        "foto": "/abogado8.jpg",
         "nombre":"Ley Garrido & Asociados",
         "direccion": "Alvaro Obregón 116, Zona Centro, 89000 Tampico, Tamps.",
         "telefono": "833 219 2240",
@@ -67,7 +67,7 @@ const datos = [
     },
     {
         "id":9,
-        "foto": "../../src/assets/abogadoexample.jpg",
+        "foto": "/abogado9.jpg",
         "nombre":"PERALES & ASOCIADOS ABOGADOS",
         "direccion": "CESAR, Gral. César López de Lara #103-DESP 1, Zona Centro, 89000 Tampico, Tamps.",
         "telefono": "833 212 2688",
@@ -75,7 +75,7 @@ const datos = [
     },
     {
         "id":10,
-        "foto": "../../src/assets/abogadoexample.jpg",
+        "foto": "/abogado10.jpg",
         "nombre":"REYES Y CIA JURIDICO",
         "direccion": "Heroes de Chapultepec 1101 ALTOS, Tamaulipas, 89060 Tampico, Tamps.",
         "telefono": "833 214 0798",
@@ -83,7 +83,7 @@ const datos = [
     },
     {
         "id":11,
-        "foto": "../../src/assets/abogadoexample.jpg",
+        "foto": "/abogado11.jpg",
         "nombre":"Lic. Víctor Zulaica - Abogado Fiscalista - Derecho Fiscal (Tributario) y Administrativo.",
         "direccion": "Universidad, U. de Wisconsin #707, 89707 Tampico, Tamps.",
         "telefono": "833 311 9572",
@@ -92,7 +92,7 @@ const datos = [
     },
     {
         "id":12,
-        "foto": "../../src/assets/abogadoexample.jpg",
+        "foto": "/abogadoexample.jpg",
         "nombre":"SALDRESE y Asociados Abogado",
         "direccion": "Benito Juárez 501-A, Zona Centro, 89000 Tampico, Tamps.",
         "telefono": "833 459 5869",
@@ -101,7 +101,7 @@ const datos = [
     },
     {
         "id":13,
-        "foto": "../../src/assets/abogadoexample.jpg",
+        "foto": "/abogado12.jpg",
         "nombre":"ABOGADOSTAM",
         "direccion": "Av. Miguel Hidalgo 5100-Piso 3, Petrolera Chairel, 89219 Tampico, Tamps.",
         "telefono": "833 325 6688",
